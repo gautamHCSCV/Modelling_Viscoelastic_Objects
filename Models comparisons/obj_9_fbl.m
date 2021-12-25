c18e1538a83e7ae3e2c652a0292134af3617e7f0
Prakash8338 <69410899+Prakash8338@users.noreply.github.com>
2021-12-25 12:11:22 +0530

    Add files via upload

---
+++ Models comparisons/obj_9_fbl.m
@@ -0,0 +1,131 @@
+k = 1;
+cl = 22;
+sf = 2;
+ 
+    load ('O9_raw.mat')
+    figure(k)
+    for phi = 14:26
+        time = 1/1000:1/1000:length(raw_pm_r{1,22}{1,phi}{1,2})/1000;
+        plot(time,raw_fm_r{1,cl}{1,phi}{1,sf})
+        xlabel('time')
+        ylabel('signals for obj9')
+        hold on
+    end
+    hold off
+   
+
+%%
+k = k+1;
+    for phi = 27:39
+        fk = raw_fm_r{1,22}{1,phi}{1,2};
+        gk = fft(fk);
+        figure(k)
+        g0k = (gk(1));
+        plot(phi,g0k,'--gs',...
+    'LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor','b',...
+    'MarkerFaceColor',[0.5,0.5,0.5])
+        xlabel('k')
+        ylabel('gok')
+        hold on
+    end
+    hold off
+    k = k+1;
+    for phi = 27:39
+        fk = raw_fm_r{1,22}{1,phi}{1,2};
+        gk = fft(fk);
+        figure(k)
+        g1k = 2*real(gk(2))/length(fk);
+        plot(phi,g1k,'--gs',...
+    'LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor','b',...
+    'MarkerFaceColor',[0.5,0.5,0.5])
+        xlabel('k')
+        ylabel('g1k')
+        hold on
+    end
+    hold off
+    k = k+1;
+    for phi =27:39
+        fk = raw_fm_r{1,22}{1,phi}{1,2};
+        gk = fft(fk);
+        figure(k)
+        g2k = -2*imag(gk(2))/length(fk);
+        plot(phi,g2k,'--gs',...
+    'LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor','b',...
+    'MarkerFaceColor',[0.5,0.5,0.5])
+        xlabel('k')
+        ylabel('g2k')
+        hold on
+    end
+    hold off
+    k = k+1;
+    for phi = 27:39
+        fk = raw_fm_r{1,22}{1,phi}{1,2};
+        gk = fft(fk);
+        figure(k)
+        g3k = 2*real(gk(3))/length(fk);
+        plot(phi,g3k,'--gs',...
+    'LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor','b',...
+    'MarkerFaceColor',[0.5,0.5,0.5])
+        xlabel('k')
+        ylabel('g3k')
+        hold on
+    end
+    hold off
+    k = k+1;
+    for phi = 27:39
+        fk = raw_fm_r{1,22}{1,phi}{1,2};
+        gk = fft(fk);
+        figure(k)
+        g4k = -2*imag(gk(3))/length(fk);
+        plot(phi,g4k,'--gs',...
+    'LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor','b',...
+    'MarkerFaceColor',[0.5,0.5,0.5])
+        xlabel('k')
+        ylabel('g4k')
+        hold on
+    end
+    hold off
+    k = k+1;
+    for phi = 27:39
+        fk = raw_fm_r{1,22}{1,phi}{1,2};
+        gk = fft(fk);
+        figure(k)
+        g5k = 2*real(gk(4))/length(fk);
+        plot(phi,g5k,'--gs',...
+    'LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor','b',...
+    'MarkerFaceColor',[0.5,0.5,0.5])
+        xlabel('k')
+        ylabel('g5k')
+        hold on
+    end
+    hold off
+    k = k+1;
+    for phi = 27:39
+        fk = raw_fm_r{1,22}{1,phi}{1,2};
+        gk = fft(fk);
+        figure(k)
+        g10k = -2*imag(gk(6))/length(fk);
+        plot(phi,g10k,'--gs',...
+    'LineWidth',2,...
+    'MarkerSize',10,...
+    'MarkerEdgeColor','b',...
+    'MarkerFaceColor',[0.5,0.5,0.5])
+        xlabel('k')
+        ylabel('g10k')
+        hold on    
+    end
+   
+   
+   
